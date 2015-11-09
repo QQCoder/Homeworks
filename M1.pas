5b6d963c1d57b062ee9f90bdf46f4d6397c2fee2
Sidikov Marsel <sidikov.marsel@gmail.com>
2015-11-09 13:07:06 +0300

    Update M1.pas

--- M1.pas
+++ M1.pas
@@ -2,11 +2,14 @@ const
  n = 3;
 
 type
+ // добавить пробел после 1..n,
  matrix = array[1..n,1..n] of integer;
 
+// функции лучше называть read_matrix, переменные тоже с маленькой - паскаль игнорирует капитализацию
 procedure readMatrix(var A: matrix);
 var i, j: integer;
 begin
+// обернуть в нормальные бегин енд здесть и везде
  for i := 1 to n do
   for j := 1 to n do readln(A[i,j]);
 end;
